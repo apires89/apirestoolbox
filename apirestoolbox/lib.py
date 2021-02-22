e4222ef8614d1a761633b11925ed51a6ad0376a6
andre <andrecabralpires@gmail.com>
2021-02-22 15:16:29 +0000

    psuh

--- apirestoolbox/lib.py
+++ apirestoolbox/lib.py
@@ -1,10 +1,17 @@
 # pylint: disable=missing-docstring
 
+import time
+import webbrowser
+import datetime
+import pandas as pd
 import sys
 import csv
 #import datetime
 import calendar
 import requests
+from os.path import split
+
+pd.set_option('display.width', 200)
 
 
 from apirestoolbox.weather import search_city
@@ -30,3 +37,41 @@ def monthly_forecast(woeid, year, month):
         forecasts = forecasts + daily_forecast(woeid, year, month, day)
     return forecasts
 
+
+
+
+
+def try_me():
+    """ Open my social network profiles
+    """
+    print("*************************************")
+    print("Hello world, welcome to my package...")
+    time.sleep(2)
+    print("******************************")
+    print("The most important thing is...")
+    time.sleep(2)
+    print("3")
+    time.sleep(2)
+    print("2")
+    time.sleep(2)
+    print("1")
+    webbrowser.open('https://www.youtube.com/watch?v=fNFzfwLM72c', new=2)
+    webbrowser.open('https://www.youtube.com/watch?v=fNFzfwLM72c', new=2)
+    webbrowser.open('https://www.youtube.com/watch?v=fNFzfwLM72c', new=2)
+    webbrowser.open('https://www.youtube.com/watch?v=fNFzfwLM72c', new=2)
+    webbrowser.open('https://www.youtube.com/watch?v=fNFzfwLM72c', new=2)
+    print("**************")
+    print("Ah ah ah ah...")
+    time.sleep(2)
+    print("***************")
+    print("Staying alive!")
+    print("***************")
+
+
+if __name__ == '__main__':
+    # For introspections purpose to quickly get this functions on ipython
+    # folder_source, _ = split(hello_world.__file__)
+    # df = pd.read_csv('{}/data/data.csv.gz'.format(folder_source))
+    # clean_data = clean_data(df)
+    # print(' dataframe cleaned')
+    try_me()
